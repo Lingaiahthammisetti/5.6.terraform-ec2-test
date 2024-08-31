dde33513010e31d9e63454c9d1c5ff628c489bed
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-31 20:07:21 +0530

    terraform msg

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 
-module  "ec2-test"{
+module "ec2-test" {
 
   source = "../5.terraform-aws-ec2"
   instance_type = "t3.small"
 
    tags = {
-     Name = "Module-Test"
+     Name = "Module-EC2-Instance-Test"
      Terraform = true
    }
 }
